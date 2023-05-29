dc5ad8f0c5d122afb27f0065293e6ebef26aa5b5
dlusquinos <129416070+dlusquinos@users.noreply.github.com>
2023-05-30 00:53:12 +0200

    Update datos.js

--- datos.js
+++ datos.js
@@ -4,3 +4,9 @@ var sales_objects = [
 	{week: '04/06/2023', sales: [{quantity: 327, average: 1.02}, {quantity: 2130, average: 1.115} ]},
 	{week: '11/06/2023', sales: [{quantity: 720, average: 1.07}, {quantity: 200, average: 2.1}, {quantity: 8000, average: 1.044}, {quantity: 200, average: 1.111} ]}	
 ]
+
+
+var users_objects = [
+	{name: 'Diego L.', user: '@diegolusquinos', scl: 20, since: '04/06/2023'},
+	{name: 'Kike', user: '@KikeMercado', scl: 15, since: '04/06/2023'}
+]
