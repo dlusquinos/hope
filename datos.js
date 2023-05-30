79bbe9b42ac4d3ffdba44a33f7419176a7c5cb38
Hopelanft <134980789+Hopelanft@users.noreply.github.com>
2023-05-30 19:52:28 -0400

    Update datos.js

--- datos.js
+++ datos.js
@@ -4,7 +4,7 @@ var sales_objects = [
 	{week: '04/06/2023', sales: [{quantity: 327, average: 1.02}, {quantity: 2130, average: 1.115} ]},
 	{week: '11/06/2023', sales: [{quantity: 720, average: 1.07}, {quantity: 200, average: 2.1}, {quantity: 8000, average: 1.044}, {quantity: 200, average: 1.111} ]},
 	{week: '12/07/2024', sales: [{quantity: 5432, average: 1.24}, {quantity: 4000, average: 1.75}, {quantity: 7000, average: 1.034}, {quantity: 500, average: 1.233} ]}
-]       
+{week: '30/05/2023', sales: [{quantity: 100, average: 1.099},]       
 
 
 var users_objects = [
